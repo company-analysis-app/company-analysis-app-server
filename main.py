cbb7e76a1e3b904a048be10f3edc9f3c49bd19d8
kyusangshim <ks030788@naver.com>
2025-06-30 14:07:08 +0900

    fix: #2 main.py에 dart 라우터 설정

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from routers import auth #, companies, users, recommendations
+from routers import auth, dart
 from database import Base, engine
 
 Base.metadata.create_all(bind=engine)
@@ -7,6 +7,7 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 app.include_router(auth.router, prefix="/auth", tags=["Auth"])
+app.include_router(dart.router, prefix='/darts', tags=["Darts"])
 # app.include_router(companies.router, prefix="/companies", tags=["Companies"])
 # app.include_router(users.router, prefix="/users", tags=["Users"])
 # app.include_router(recommendations.router, prefix="/recommendations", tags=["Recommendations"])
