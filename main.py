260584ab457d1683ddf2c0fa8a04aad8d2b39fd7
HanbinSeong <briquetroad@gmail.com>
2025-06-30 19:55:25 +0900

    fix: #12 mege 코드 충돌 수정

--- main.py
+++ main.py
@@ -1,29 +1,30 @@
 from fastapi import FastAPI, Request, Response
-from routers import auth, users, dart
+from routers import auth, users, dart, naver_news
 from database import Base, engine
 from starlette.middleware.sessions import SessionMiddleware
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 import os
 
-load_dotenv(override=True)  # .env 파일 읽기(덮어쓰기 허용)
+load_dotenv(override=True)
 
 app = FastAPI()
 
 origins = [
     "http://localhost:3000",
-    "http://localhost:3000/",      # 슬래시 포함
+    "http://localhost:3000/",
     "http://127.0.0.1:3000",      
     "http://127.0.0.1:3000/",      
 ]
+app = FastAPI()
 
 app.add_middleware(SessionMiddleware, secret_key=os.getenv("SECRET_KEY"))
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,  # 특정 출처의 도메인을 허용
-    allow_credentials=True,  # 로그인 세션유지, 인증 토큰 사용 시 true
-    allow_methods=["*"],  # GET, PUT, POST, DELETE
-    allow_headers=["*"],  # 대표적인 예) Authorization, X-Custom-Header
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 @app.middleware("http")
@@ -40,4 +41,4 @@ Base.metadata.create_all(bind=engine)
 app.include_router(auth.router, prefix="/auth", tags=["Auth"])
 app.include_router(users.router, prefix="/users", tags=["Users"])
 app.include_router(dart.router, prefix='/darts', tags=["Darts"])
-app.include_router(naver_news.router)
+app.include_router(naver_news.router, prefix="/naver", tags=["Naver News"])
