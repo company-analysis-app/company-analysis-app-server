2e986607a9ce774e10821ca7f4baa5815c679324
HanbinSeong <briquetroad@gmail.com>
2025-06-30 12:31:36 +0900

    fix: #1 선호 항목, 생성, 수정시간 관련 컬럼 추가

--- schemas/user.py
+++ schemas/user.py
@@ -1,18 +1,33 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from datetime import datetime
+from typing import List, Optional
 
-
+# 공통 필드
 class UserBase(BaseModel):
     email: str
     name: str
 
-
+# 사용자 생성 시 입력
 class UserCreate(UserBase):
     oauth_provider: str
     oauth_sub: str
+    preferences: Optional[List[str]] = Field(default_factory=list)
+    favorites: Optional[List[int]] = Field(default_factory=list)
 
+# 사용자 업데이트 시 입력
+class UserUpdate(BaseModel):
+    preferences: Optional[List[str]] = None
+    favorites: Optional[List[int]] = None
 
+# 응답용 스키마
 class UserOut(UserBase):
     id: int
+    oauth_provider: str
+    oauth_sub: str
+    preferences: List[str]
+    favorites: List[int]
+    created_at: datetime
+    updated_at: datetime
 
     class Config:
         orm_mode = True
