f8840a31a259960f9141438873c715db798192ad
kyusangshim <ks030788@naver.com>
2025-07-02 11:06:21 +0900

    feature: #26 라우터 일단 간단히 수정

--- routers/dart_search.py
+++ routers/dart_search.py
@@ -11,19 +11,31 @@ DART_API = os.getenv("dart_api_key")
 router = APIRouter()
 
 @router.get("/")
-def get_bords(name: str, db: Session=Depends(get_db)):
+def get_bords(keyword: str, db: Session=Depends(get_db)):
     darts = db.query(
         Darts.corp_code,
-    ).filter(Darts.corp_name == name).first()
+        Darts.corp_name,
+    ).filter(
+        Darts.corp_name.ilike(f"%{keyword}%")
+    ).order_by(
+        Darts.corp_name.asc()
+    ).all()
 
     if darts is None:
         return {"message": "해당 회사명을 찾을 수 없습니다."}
+    
+    result_list = []
+    for row in darts:
+        result_list.append({
+            "corp_code": row.corp_code,
+            "corp_name": row.corp_name
+        })
 
-    return darts[0]
+    return result_list
 
 
-@router.get("/getInfos")
-def get_infos(code: str, db:Session=Depends(get_db)):
-    result = db.query(CompanyOverviews).filter(CompanyOverviews.corp_code == code).first()
-    return result
+# @router.get("/getInfos")
+# def get_infos(code: str, db:Session=Depends(get_db)):
+#     result = db.query(CompanyOverviews).filter(CompanyOverviews.corp_code == code).first()
+#     return result
 
