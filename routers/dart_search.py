e129d3329b7de1a9e5fc006f39a5c606cafd372c
kyusangshim <ks030788@naver.com>
2025-07-02 09:59:30 +0900

    feature: #26 routers > dart_search.py 구조 정의

---
+++ routers/dart_search.py
@@ -0,0 +1,29 @@
+from fastapi import Depends, APIRouter
+from sqlalchemy.orm import Session
+from models.dart import Darts
+from models.company_overview import CompanyOverviews
+from database import get_db
+import os
+
+
+DART_API = os.getenv("dart_api_key")
+
+router = APIRouter()
+
+@router.get("/")
+def get_bords(name: str, db: Session=Depends(get_db)):
+    darts = db.query(
+        Darts.corp_code,
+    ).filter(Darts.corp_name == name).first()
+
+    if darts is None:
+        return {"message": "해당 회사명을 찾을 수 없습니다."}
+
+    return darts[0]
+
+
+@router.get("/getInfos")
+def get_infos(code: str, db:Session=Depends(get_db)):
+    result = db.query(CompanyOverviews).filter(CompanyOverviews.corp_code == code).first()
+    return result
+
