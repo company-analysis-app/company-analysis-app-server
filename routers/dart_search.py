38edd6ffc16422e58426369c299a53be57a23473
kyusangshim <ks030788@naver.com>
2025-07-02 21:50:16 +0900

    feature: #32 favorite_count 가장 높은 상위 3개 불러오는 라우터 추가

--- routers/dart_search.py
+++ routers/dart_search.py
@@ -1,6 +1,7 @@
 from fastapi import Depends, APIRouter
 from sqlalchemy.orm import Session
 from models.dart import Darts
+from models.company_overview import CompanyOverviews
 from database import get_db
 import os
 
@@ -25,6 +26,32 @@ def get_bords(keyword: str, db: Session=Depends(get_db)):
     
     result_list = []
     for row in darts:
+        result_list.append({
+            "corp_code": row.corp_code,
+            "corp_name": row.corp_name
+        })
+
+    return result_list
+
+
+@router.get("/bestCompanies")
+def get_best_companies(db: Session=Depends(get_db)):
+    best_results = (
+        db.query(
+            Darts.corp_code,
+            Darts.corp_name,
+        )
+        .join(CompanyOverviews, Darts.corp_code == CompanyOverviews.corp_code)
+        .order_by(CompanyOverviews.favorite_count.desc())
+        .limit(3)
+        .all()
+    )
+
+    if not best_results:
+        return {"message": "해당 회사명을 찾을 수 없습니다."}
+    
+    result_list = []
+    for row in best_results:
         result_list.append({
             "corp_code": row.corp_code,
             "corp_name": row.corp_name
